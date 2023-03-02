8d452d394a445c4c237f76f9b626aff0f450f003
Gabriel Costa <garccosta@gmail.com>
2023-03-01 21:40:13 -0300

    feat: add JS website and logic

---
+++ public/main.js
@@ -0,0 +1,28 @@
+async function init() {
+  let rustApp = null 
+  
+  try {
+    rustApp = await import('../pkg')
+  } catch (error) {
+    console.error(error);
+    return;
+  }
+
+  const input = document.getElementById('upload');
+  const fileReader = new FileReader()
+
+  fileReader.onloadend = () => {
+    let base64 = fileReader.result.replace(
+      /^data:image\/(png|jpeg|jpg);base64,/, ''
+    )
+  
+    rustApp.grayscale(base64)
+  }
+
+  input.addEventListener('change', () => {
+    fileReader.readAsDataURL(input.files[0])
+  })
+
+}
+
+init()
