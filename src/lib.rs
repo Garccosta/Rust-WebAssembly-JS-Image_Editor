93e405f9ffa91326c71af948ddf59bd82c6feb57
Gabriel Costa <garccosta@gmail.com>
2023-03-01 22:01:04 -0300

    feat: add image crate and grayscale edition to uploaded image

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,18 @@
 use wasm_bindgen::prelude::*;
 use web_sys::console::log_1 as log;
 use base64::decode;
+use image::load_from_memory;
 
 #[wasm_bindgen]
 pub fn grayscale(encoded_file: &str) {
   log(&"Grayscale called".into());
 
   let base64_to_vector= decode(encoded_file).unwrap();
-  log(&"Image decoded".into())
+  log(&"Image decoded".into());
+
+  let mut img = load_from_memory(&base64_to_vector).unwrap();
+  log(&"Image loaded".into());
+
+  img = img.grayscale();
+  log(&"Grayscale effect applied".into());
 }
