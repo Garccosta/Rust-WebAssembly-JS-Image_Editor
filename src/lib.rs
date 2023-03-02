a67c0ebdb7ae7ee8a20d0902bd03564bf0e5a6e2
Gabriel Costa <garccosta@gmail.com>
2023-03-01 22:12:35 -0300

    feat: add conversion back to base64 for rust function response

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
 use wasm_bindgen::prelude::*;
 use web_sys::console::log_1 as log;
-use base64::decode;
+use base64::{encode, decode};
 use image::load_from_memory;
 use image::ImageOutputFormat::Png;
 
 #[wasm_bindgen]
-pub fn grayscale(encoded_file: &str) {
+pub fn grayscale(encoded_file: &str) -> String {
   log(&"Grayscale called".into());
 
   let base64_to_vector= decode(encoded_file).unwrap();
@@ -21,4 +21,11 @@ pub fn grayscale(encoded_file: &str) {
   img.write_to(&mut buffer, Png).unwrap();
   log(&"New image written".into());
 
+  let encoded_img = encode(&buffer);
+  let data_url = format!(
+    "data:image/png;base64,{}",
+    encoded_img
+  );
+
+  data_url
 }
